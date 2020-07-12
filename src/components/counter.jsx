6d503fcd529719ab73777114e4613fa349a39e64
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2020-07-12 19:22:15 +0600

    array render

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,12 +3,18 @@ class Counter extends Component {
   state = {
     count: 0,
     imageUrl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
   };
   styles = {
     fontSize: 10,
     fontWeight: "bold",
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return <ul>{this.renderTags()}</ul>;
+  }
 
   render() {
     return (
@@ -18,11 +24,7 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <ul>{this.renderTags()}</ul>
       </div>
     );
   }
