2bc54c789b4862d3de16dab627541caa726a6f64
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2020-07-12 17:50:14 +0600

    State, Class , Bootstrap, Component

---
+++ src/components/counter.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
+  };
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
+
+  render() {
+    return (
+      <div>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 ";
+    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
+
+ReactDom.render(<Counter />, document.getElementById("root"));
